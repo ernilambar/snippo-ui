3bae34a1f7337199deb82391e72b9ad4c87e684b
Nilambar Sharma <nilambar@outlook.com>
2025-07-18 15:16:17 +0545

    Update css

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,24 +1,24 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    sourcemap: true,
-    lib: {
-      entry: 'react/index.js',
-      name: 'SnippoUI',
-      fileName: (format) => `snippo-ui.${format}.js`,
-      formats: ['es', 'cjs'],
-    },
-    rollupOptions: {
-      external: ['react', 'react-dom'],
-      output: {
-        globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM',
-        },
-      },
-    },
-  },
-});
+export default defineConfig( {
+	plugins: [ react() ],
+	build: {
+		sourcemap: true,
+		lib: {
+			entry: 'react/index.js',
+			name: 'SnippoUI',
+			fileName: ( format ) => `snippo-ui.${ format }.js`,
+			formats: [ 'es', 'cjs' ],
+		},
+		rollupOptions: {
+			external: [ 'react', 'react-dom' ],
+			output: {
+				globals: {
+					react: 'React',
+					'react-dom': 'ReactDOM',
+				},
+			},
+		},
+	},
+} );
