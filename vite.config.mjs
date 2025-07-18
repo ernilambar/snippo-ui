af7bbbf442e1db08909b0d859884739e3181c510
Nilambar Sharma <nilambar@outlook.com>
2025-07-18 13:34:59 +0545

    Bundle

---
+++ vite.config.mjs
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    lib: {
+      entry: 'react/SnippetsApp.jsx',
+      name: 'SnippetsApp',
+      fileName: (format) => `SnippetsApp.${format}.js`,
+      formats: ['es', 'cjs'],
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+  },
+});
